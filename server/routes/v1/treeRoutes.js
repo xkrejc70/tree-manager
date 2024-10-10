3ab9153b81b6eb42132483ed58619533c26e3542
hojzas <hojzas@seznam.cz>
2024-10-10 15:31:27 +0200

    Vue cleanup

--- server/routes/v1/treeRoutes.js
+++ server/routes/v1/treeRoutes.js
@@ -2,7 +2,7 @@ import express from 'express'
 import { getTree, addNode } from '../../controllers/nodeController.js'
 const router = express.Router()
 
-router.get('/tree', getTree)
+router.get('/getTree', getTree)
 
 router.post('/addNode', addNode)
 
